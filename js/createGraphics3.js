11b2a45aed48b1d6ec99d7321bc3a003b3b76811
J Kangas <jussi.v.kangas@gmail.com>
2021-04-09 16:15:23 +0200

    Definitions modul tillagd

--- js/createGraphics3.js
+++ js/createGraphics3.js
@@ -1335,11 +1335,70 @@ function createStationPlot(){
           /*var range = [];
             for (let i = 0; i < names.length; i++) {
                 range.push(i/(names.length-1))
-            }*/   
+            }  */
+        
+            /*function shiftColor(h, shift) {
+                let r = 0, g = 0, b = 0;
+              
+                // 3 digits
+                if (h.length == 4) {
+                  r = "0x" + h[1] + h[1];
+                  g = "0x" + h[2] + h[2];
+                  b = "0x" + h[3] + h[3];
+              
+                // 6 digits
+                } else if (h.length == 7) {
+                  r = "0x" + h[1] + h[2];
+                  g = "0x" + h[3] + h[4];
+                  b = "0x" + h[5] + h[6];
+                }
+
+                var rgb = "rgb("+ +r + "," + +g + "," + +b + ")";
+                var val = rgb.split(/[(]|[)]|[,]/).slice(1,4);
+
+                if(shift== -60){
+                    console.log(val);
+                }
+                
+                r = (+parseFloat(val[0],10)+shift).toString(16);
+                g = (+parseFloat(val[1],10)+shift).toString(16);
+                b = (+parseFloat(val[2],10)+shift).toString(16);
+
+                if (r.length == 1)
+                    r = "0" + r;
+                if (g.length == 1)
+                    g = "0" + g;
+                if (b.length == 1)
+                    b = "0" + b;
+
+                return "#" + r + g + b;
+              }*/
+        
+        /*var names={}
+        var count=0;
+        var colorCount=0;
+        c=d3.schemeTableau10;
+        console.log(c);
+          for(let j=0; j<stations.length; j++){
+              if((j%10) == 0){
+                var s = -60+count;
+                count +=20;
+              }
+              if(colorCount==10){
+                  colorCount=0;
+              }
+              names[stations[j].Name] =shiftColor(c[colorCount],s);
+              colorCount +=1;
+          }
+
+          console.log(names);*/
     
           
         //var scale = d3.scaleOrdinal(names,range);
-        //var color = d3.interpolateRainbow;
+
+        /*function color(val) {
+            return names[val];
+        };*/
 
          var color=d3.scaleOrdinal( 
             names,
